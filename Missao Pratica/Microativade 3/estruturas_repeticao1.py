37653a490df0210aad585ac0df5ef7ca1bbccb04
Pedro Henrique <165848535+PedroHNRQE@users.noreply.github.com>
2024-07-31 13:44:49 -0300

    Add files via upload

---
+++ Missao Pratica/Microativade 3/estruturas_repeticao1.py
@@ -0,0 +1,6 @@
+entrada_idade = ''
+
+while entrada_idade != 0:
+    entrada_idade = int(input('Digite um numero qualquer ou 0 para sair: '))
+    print('Numero digitado: {}'.format(entrada_idade))
+print('Fim')
