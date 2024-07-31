0917b09ebd5ae8adb40bfa6d4c888d3a371e0fb8
Pedro Henrique <165848535+PedroHNRQE@users.noreply.github.com>
2024-07-31 13:47:27 -0300

    Delete Missao Pratica - Nivel 2 directory

--- Missao Pratica - Nivel 2/Microativade 3/estruturas_repeticao1.py
+++
@@ -1,6 +0,0 @@
-entrada_idade = ''
-
-while entrada_idade != 0:
-    entrada_idade = int(input('Digite um numero qualquer ou 0 para sair: '))
-    print('Numero digitado: {}'.format(entrada_idade))
-print('Fim')
